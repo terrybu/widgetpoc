98bd62a5ffaad910875f7211711b301817424403
Terry Bu <terry.bu@verizonwireless.com>
2017-05-25 14:19:59 -0400

    Initial Commit

---
+++ WidgetPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WidgetPOC
+//
+//  Created by Bu, Terry on 5/25/17.
+//  Copyright © 2017 Bu, Terry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
